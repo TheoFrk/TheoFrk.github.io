e3a263f7ef374f6c5599ad908b50fbd0276d8b20
TheoFrk <33786175+TheoFrk@users.noreply.github.com>
2018-11-10 13:33:55 +0100

    Add files via upload

---
+++ sites/js.js
@@ -0,0 +1,12 @@
+var WortDesTages = 'Doublehyped-Oldies';   //Hier
+
+
+function WortDesTagesf() {
+    window.open('https://www.wikipedia.org/wiki/' + WortDesTages, '_blank');
+}
+
+function Input() {
+    WortDesTages = document.getElementById('UserInput').value;
+    var elem = document.getElementById('WordDesTages');
+    elem.innerHTML = WortDesTages;
+}
